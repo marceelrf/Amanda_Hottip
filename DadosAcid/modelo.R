ab3022a7508eb30d979bd3a9415eb55fe5a8290d
Marcel Ferreira <marcelrodrigues45@yahoo.com.br>
2024-01-16 22:28:26 -0300

    update

--- DadosAcid/modelo.R
+++ DadosAcid/modelo.R
@@ -9,4 +9,6 @@ dados_acid %>%
   mutate(anova = map(data, ~aov(sev~brand_name + treatments,
                                 data= .x))) %>% 
   mutate(anova_tidy = map(anova, broom::tidy)) %>% 
-  unnest(anova_tidy)
+  unnest(anova_tidy) %>% 
+  select(1,2,6) %>% 
+  rename("study" = 1, "year" = 2, "v_sev" = 3)
