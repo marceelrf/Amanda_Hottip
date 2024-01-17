9c8fba7e4ecc40ab6a1b33ad28a9fff7ed741807
Marcel Ferreira <marcelrodrigues45@yahoo.com.br>
2024-01-16 22:26:53 -0300

    ffhs

--- vivi_tmp.R
+++ vivi_tmp.R
@@ -6,3 +6,22 @@ unique(mica@data$mature_mirna_id)
 micb <- get_multimir(target = 'MICB',org = "hsa",table = "validated")
 
 unique(micb@data$mature_mirna_id)
+
+
+
+(
+  syn_data <- tibble(x = 1:1000,
+                     #y = rgamma(1000,shape = 1),
+                     val1 = stringi::stri_rand_strings(1000,
+                                                       length = 1,
+                                                       pattern = "[A-E]"),
+                     val2 = rbinom(n = 1000,size = 1,prob = .75)
+                     )
+  )
+
+syn_data %>% 
+  mutate(y = sin(x)) %>% 
+  ggplot(aes(x = x, y = y, color = factor(val2))) +
+  geom_point() +
+  ggrepel::geom_label_repel(aes(label = val1))
+  theme_bw()
